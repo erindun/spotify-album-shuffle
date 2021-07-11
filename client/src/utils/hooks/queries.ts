8e91ede5327d685d0a9b5744a61e96992bebb45c
Garrett Duncan <garrettdunc@gmail.com>
2021-07-10 23:41:32 -0400

    Change accessToken key name

--- client/src/utils/hooks/queries.ts
+++ client/src/utils/hooks/queries.ts
@@ -3,12 +3,8 @@ import { useQuery, UseQueryResult } from 'react-query';
 import { fetchAccessToken } from '../api';
 
 export function useAccessTokenQuery(): UseQueryResult<AccessToken, Error> {
-  return useQuery<AccessToken, Error>(
-    'access_token',
-    () => fetchAccessToken(),
-    {
-      refetchInterval: 1000 * 3600, // 1 hour
-      refetchIntervalInBackground: true,
-    }
-  );
+  return useQuery<AccessToken, Error>('accessToken', () => fetchAccessToken(), {
+    refetchInterval: 1000 * 3600, // 1 hour
+    refetchIntervalInBackground: true,
+  });
 }
