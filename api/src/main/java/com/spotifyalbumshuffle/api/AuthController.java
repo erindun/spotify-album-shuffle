27df741b7c950f1121b05ff232e6f6dcf627db87
Garrett Duncan <garrettdunc@gmail.com>
2021-07-24 12:36:19 -0400

    Get auth code credentials

--- api/src/main/java/com/spotifyalbumshuffle/api/AuthController.java
+++ api/src/main/java/com/spotifyalbumshuffle/api/AuthController.java
@@ -1,7 +1,9 @@
 package com.spotifyalbumshuffle.api;
 
+import com.wrapper.spotify.model_objects.credentials.AuthorizationCodeCredentials;
 import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.wrapper.spotify.SpotifyApi;
 
@@ -10,24 +12,24 @@ import java.net.URI;
 @RestController
 public class AuthController {
     private static final SpotifyApi spotifyApi = new SpotifyApi.Builder()
-            .setClientId("472ab70014b14093b29c76597187f2d0")
-            .setClientSecret("05fd13c211df4bf2a36fdff06d85efb1")
-            .setRedirectUri(URI.create("http://localhost:5000/api/auth/callback"))
+            .setClientId(System.getenv("SPOTIFY_CLIENT_ID"))
+            .setClientSecret(System.getenv("SPOTIFY_CLIENT_SECRET"))
+            .setRedirectUri(URI.create(System.getenv("SPOTIFY_REDIRECT_URI")))
             .build();
 
     private static final AuthorizationCodeUriRequest authorizationCodeUriRequest = spotifyApi.authorizationCodeUri()
-            .scope("user-read-birthdate,user-read-email")
+            .scope("streaming,user-read-email,user-read-private,user-library-read,user-library-modify,user-read-playback-state,user-modify-playback-state")
             .build();
 
     @GetMapping("/auth")
-    public URI getAuthUrl() {
-        var uri = authorizationCodeUriRequest.execute();
-        System.out.println(uri);
-        return uri;
+    public URI getAuthorizationCodeUri() {
+        return authorizationCodeUriRequest.execute();
     }
 
     @GetMapping("/auth/callback")
-    public void createSession() {
-
+    public AuthorizationCodeCredentials createSession(@RequestParam String code) throws Exception {
+        var authorizationCodeRequest = spotifyApi.authorizationCode(code).build();
+        var authorizationCodeCredentials = authorizationCodeRequest.execute();
+        return authorizationCodeCredentials;
     }
 }
