05682b131c89161fee7136a42bd4b73e12a8439e
Garrett Duncan <garrettdunc@gmail.com>
2021-08-06 22:33:21 -0400

    Replace axios calls with fetch API

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,43 +1,49 @@
-import axios from 'axios';
 import { Album } from 'common';
 
 /** Fetch the current user's Spotify API access token. */
 export async function fetchAccessToken(): Promise<string> {
   try {
-    const response = await axios.get<string>('/api/auth/token', {
-      withCredentials: true,
+    const response = await fetch('/api/auth/token', {
+      credentials: 'same-origin',
     });
-    return response.data;
-  } catch {
-    throw new Error('Unable to retrieve access token');
+    if (!response.ok) throw response;
+    // Use `Response.text`, because `Response.json`
+    // is unable to handle null responses and
+    // `/api/auth/token` returns null if no token
+    // for the user is available.
+    return response.text();
+  } catch (err) {
+    throw new Error(err.message);
   }
 }
 
 /** Fetch the URL for the application's Spotify auth login. */
 export async function fetchAuthUrl(): Promise<string> {
   try {
-    const response = await axios.get<string>('/api/auth/code-uri');
-    return response.data;
-  } catch {
-    throw new Error("Can't connect to server");
+    const response = await fetch('/api/auth/code-uri');
+    if (!response.ok) throw response;
+    return response.json();
+  } catch (err) {
+    throw new Error(err.message);
   }
 }
 
 /** Log out of current user. */
 export async function logout(): Promise<void> {
-  await axios.get('/api/auth/logout', {
-    withCredentials: true,
+  await fetch('/api/auth/logout', {
+    credentials: 'same-origin',
   });
 }
 
 /** Fetch all albums in the user's Spotify library. */
 export async function fetchAlbumsList(): Promise<Album[]> {
   try {
-    const response = await axios.get<Album[]>('/api/albums', {
-      withCredentials: true,
+    const response = await fetch('/api/albums', {
+      credentials: 'same-origin',
     });
-    return response.data;
-  } catch {
-    throw new Error('Unable to get albums');
+    if (!response.ok) throw response;
+    return await response.json();
+  } catch (err) {
+    throw new Error(err.message);
   }
 }
