79a2ac5e489b205504c569676b776a409092014a
Garrett Duncan <garrettdunc@gmail.com>
2021-08-06 21:31:14 -0400

    Rename `useAccessTokenQuery` to `useAccessToken`

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
 import { Box, Spinner } from '@chakra-ui/react';
 import { Player } from './components/Player';
 import { Login } from './components/Login';
-import { useAccessTokenQuery } from './utils/hooks/queries';
+import { useAccessToken } from './utils/hooks/queries';
 
 function App(): JSX.Element {
-  const { data: accessToken, isLoading } = useAccessTokenQuery();
+  const { data: accessToken, isLoading } = useAccessToken();
 
   if (isLoading) return <Spinner />;
 
--- client/src/utils/hooks/queries.ts
+++ client/src/utils/hooks/queries.ts
@@ -1,7 +1,7 @@
 import { useQuery, UseQueryResult } from 'react-query';
 import { fetchAccessToken } from '../api';
 
-export function useAccessTokenQuery(): UseQueryResult<string, Error> {
+export function useAccessToken(): UseQueryResult<string, Error> {
   return useQuery<string, Error>('accessToken', () => fetchAccessToken(), {
     refetchInterval: (1000 * 3600) / 2, // 30 minutes
     refetchIntervalInBackground: true,
